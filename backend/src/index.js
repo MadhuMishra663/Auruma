80cf260be68c4b59e4b8dc981117f0efc3223b68
MadhuMishra663 <madhumishra663@gmail.com>
2024-11-11 14:03:53 +0530

    errorHandling

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,23 @@
 const dotenv=require('dotenv');
 const connectDB = require("./db/db");
+const express=require("express");
+const app=express();
+
 dotenv.config();
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+                    console.log("Error",erroe);
+                    throw error;
+                });
+    app.listen(process.env.PORT|| 8000,()=>{
+        console.log(`Server is running at port:${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log('MONGODB CONNECTION FAILED!',err);
+    
+})
 
 // const express=require("express");
 // const app=express();
